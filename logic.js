3d5a70b406a9666f222b61c3610609d29f01b5ff
LrE <lrespino@gmail.com>
2019-06-02 23:04:24 -0400

    js logic update

--- logic.js
+++ logic.js
@@ -1,19 +1,64 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/6.1.0/firebase-app.js"></script>
+firebase.initializeApp(config);
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#config-web-app -->
+var dataRef = firebase.database();
 
+// Initial Values
+var train = "";
+var destination = "";
+var firstTrain = 0;
+var frequency = 0;
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyC-3XU-e8HRsJP_Tm-O8v682Z3XeB7kIVM",
-    authDomain: "trainscheduler-b15f3.firebaseapp.com",
-    databaseURL: "https://trainscheduler-b15f3.firebaseio.com",
-    projectId: "trainscheduler-b15f3",
-    storageBucket: "trainscheduler-b15f3.appspot.com",
-    messagingSenderId: "735632129303",
-    appId: "1:735632129303:web:f4b6dab92f90cac2"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Capture Button Click
+$("#add-train").on("click", function(event) {
+  event.preventDefault();
+
+  // YOUR TASK!!!
+  // Code in the logic for storing and retrieving the most recent user.
+  // Don't forget to provide initial data to your Firebase database.
+  name = $("#train-input").val().trim();
+  email = $("#destination-input").val().trim();
+  age = $("#firstTrain-input").val().trim();
+  comment = $("#frequency-input").val().trim();
+
+  // Code for the push
+  dataRef.ref().push({
+
+    train: train,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
+});
+
+// Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+dataRef.ref().on("child_added", function(childSnapshot) {
+
+  // Log everything that's coming out of snapshot
+  console.log(childSnapshot.val().name);
+  console.log(childSnapshot.val().name);
+  console.log(childSnapshot.val().email);
+  console.log(childSnapshot.val().age);
+  console.log(childSnapshot.val().comment);
+  console.log(childSnapshot.val().joinDate);
+
+  // full list of items to the well
+//   $("#full-member-list").append("<div class='well'><span class='member-name'> " +
+//     childSnapshot.val().name +
+//     " </span><span class='member-email'> " + childSnapshot.val().email +
+//     " </span><span class='member-age'> " + childSnapshot.val().age +
+//     " </span><span class='member-comment'> " + childSnapshot.val().comment +
+//     " </span></div>");
+
+  // Handle the errors
+// }, function(errorObject) {
+//   console.log("Errors handled: " + errorObject.code);
+// });
+
+// dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+//   // Change the HTML to reflect
+//   $("#name-display").text(snapshot.val().name);
+//   $("#email-display").text(snapshot.val().email);
+//   $("#age-display").text(snapshot.val().age);
+//   $("#comment-display").text(snapshot.val().comment);
+// });
