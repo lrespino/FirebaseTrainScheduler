76d566ed51d2989f88f1e087aac2794310811c62
LrE <lrespino@gmail.com>
2019-06-02 22:56:51 -0400

    html update, js int

---
+++ logic.js
@@ -0,0 +1,19 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.1.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC-3XU-e8HRsJP_Tm-O8v682Z3XeB7kIVM",
+    authDomain: "trainscheduler-b15f3.firebaseapp.com",
+    databaseURL: "https://trainscheduler-b15f3.firebaseio.com",
+    projectId: "trainscheduler-b15f3",
+    storageBucket: "trainscheduler-b15f3.appspot.com",
+    messagingSenderId: "735632129303",
+    appId: "1:735632129303:web:f4b6dab92f90cac2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
